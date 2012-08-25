fcd5f62c73cbb3475780d0c65190b8af64385068
Jason Cooper <jase.coop@gmail.com>
2012-08-25 22:30:00 +0100

    Design update

--- source/javascripts/scripts.js
+++ source/javascripts/scripts.js
@@ -25,14 +25,130 @@ var colours = ["blue"];
 
 }
 
+function asideToggle() {
+
+	$("article.content aside a").click(function(e) {
+
+    	e.preventDefault();
+
+
+    	if ($(".open").length) {
+
+    		$("body").removeClass("open");
+    		$(this).closest('aside').attr('style','bottom: -250px;');
+
+    	}	else {
+
+    		$("body").addClass("open");
+    		$(this).closest('aside').attr('style','bottom: 0px;');
+
+    	}
+
+	});
+
+}
+
+function fixedAsideOpen() {
+
+	var winTop = $(window).scrollTop();
+	var halfWinTop = winTop / 2;
+
+	$('article.content').each(function(){
+
+		var xTop = $(this).offset().top;
+		var xBottom = xTop + $(this).height();
+
+		if (xTop <= winTop && xBottom >= winTop) {
+			$(this).addClass('active');
+		} else {
+			$(this).removeClass('active');
+			$(this).children('.info').attr('style','bottom: -300px;');
+		}
+
+	});
+
+	if($('.active').length){
+
+		var src = $(this).scrollTop();
+		var offset = $('.active').offset();
+
+		$('.aside.sleep').attr('style','');
+
+		if (src >= offset.top){
+		
+			$('.active').children('.info').css('bottom','0');
+
+		} else {
+        
+    	$('.active').children('.info').attr('style','');
+    	
+    }
+
+	}
+
+
+}
+
+function fixedAsideClosed() {
+
+	var winTop = $(window).scrollTop();
+	var halfWinTop = winTop / 2;
+
+	$('article.content').each(function(){
+
+		var xTop = $(this).offset().top;
+		var xBottom = xTop + $(this).height();
+
+		if (xTop <= winTop && xBottom >= winTop) {
+			$(this).addClass('active');
+		} else {
+			$(this).removeClass('active');
+			$(this).children('.info').attr('style','bottom: -300px;');
+		}
+
+	});
+
+	if($('.active').length){
+
+		var src = $(this).scrollTop();
+		var offset = $('.active').offset();
+
+		$('.aside.sleep').attr('style','');
+
+		if (src >= offset.top){
+		
+			$('.active').children('.info').css('bottom','-250px');
+
+		} else {
+        
+    	$('.active').children('.info').attr('style','-300px');
+    	
+    }
+
+	}
+
+}
+
+
+
+$(window).scroll(function(){
+	
+	if ($(".open").length){
+
+		fixedAsideOpen();
+
+	} else {
+
+		fixedAsideClosed();
+
+	}
+
+});
+
 
 $(document).ready(function() {
 
-    if ($('#content').hasClass('home')) {
-
-	background();
-	indexClick();
-
-    }
+  asideToggle();
+  background();
 
 });
